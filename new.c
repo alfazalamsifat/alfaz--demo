b766cfa0527207a6108c95c08c2c7ce4b65bfdfd
alfazalamsifat <sifatalfazalam@gmail.com>
2025-05-19 03:05:15 +0600

    edit something in c

---
+++ new.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   printf("Hello, World!\n");
+   printf("This is a new file.\n");
+    printf("This is a new line.\n");
+    printf("%d\n", 5);
+    return 0;
+}
